9dc2f822e091f115be997599dfc94f933bb0c550
VladBreaban <vladbreaban@yahoo.com>
2022-06-08 18:06:28 +0100

    comment

--- DataManager/Models/RealEstate_AppContext.cs
+++ DataManager/Models/RealEstate_AppContext.cs
@@ -27,6 +27,7 @@ namespace DataManager.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
+                // to do, move it to an appsettings 
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=DESKTOP-OIPLHJT;Database=RealEstate_App;Trusted_Connection=True;");
             }
