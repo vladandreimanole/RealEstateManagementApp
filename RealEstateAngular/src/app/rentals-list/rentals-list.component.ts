8ff1355fa70d22d583e97c9532567ec108f64e15
vladandreimanole <vlad.manole@relevance.ro>
2022-06-22 01:03:30 +0300

    contracts page click event

--- RealEstateAngular/src/app/rentals-list/rentals-list.component.ts
+++ RealEstateAngular/src/app/rentals-list/rentals-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ContractModel } from '../models/Contract';
 import { DataService } from '../services/data-service.service';
 
@@ -9,10 +10,12 @@ import { DataService } from '../services/data-service.service';
 })
 export class RentalsListComponent implements OnInit {
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private router: Router) { }
   public contracts: ContractModel[] = [];
   async ngOnInit(): Promise<void> {
     this.contracts = (await this.dataService.getContractByLandlordId(Number(localStorage.getItem("userId"))))!;
   }
-
+  openContractPage(contractId: number){
+    this.router.navigate(["contract/" + contractId.toString()]);
+  }
 }
